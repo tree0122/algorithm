e612cfd506101d35e6ecb9ee1e1d50a1ce36e69f
tree0122 <cuiyaohua11@163.com>
2019-12-20 20:41:31 +0800

    leetcode: 3Sum better

--- src/main/java/com/tree/core/algorithm/leetcode/P0015ThreeSum.java
+++ src/main/java/com/tree/core/algorithm/leetcode/P0015ThreeSum.java
@@ -102,6 +102,43 @@ public class P0015ThreeSum {
         return ans;
     }
 
-
+    public List<List<Integer>> better(int[] nums){
+        Arrays.sort(nums);
+        ArrayList<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > 0){
+                break;
+            }
+            if (i != 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+            int target = 0 - nums[i];
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k){
+                if (nums[j] + nums[k] == target){
+                    //add
+                    ArrayList<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(nums[k]);
+                    ans.add(list);
+                    while (j < k && nums[j] == nums[j + 1]){
+                        j++;
+                    }
+                    while (j < k & nums[k] == nums[k - 1]){
+                        k--;
+                    }
+                    i++;
+                    k--;
+                }else if (nums[j] + nums[k] < target){
+                    j++;
+                }else {
+                    k--;
+                }
+            }
+        }
+        return ans;
+    }
 
 }
