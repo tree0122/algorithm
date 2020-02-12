2aa07ef891aff8e26ee981df795e07c26d0ca651
tree0122 <cuiyaohua11@163.com>
2020-02-12 16:01:07 +0800

    zuogod: merge sort

---
+++ src/main/java/com/tree/core/algorithm/newcoder/zuogod/x04/junior/Q010107Recurse.java
@@ -0,0 +1,22 @@
+package com.tree.core.algorithm.newcoder.zuogod.x04.junior;
+
+
+/**
+ * 剖析递归行为和递归行为时间复杂度的估算
+ *
+ * 一个递归行为的例子
+ *
+ * master公式的使用
+ * T(N) = a*T(N/b) + O(N^d)
+ * 1) log(b,a) > d -> 复杂度为O(N^log(b,a))
+ * 2) log(b,a) = d -> 复杂度为O(N^d * logN)
+ * 3) log(b,a) < d -> 复杂度为O(N^d)
+ *
+ * 补充阅读:www.gocalf.com/blog/algorithm-complexity-and-master-
+ * theorem.html
+ */
+public class Q010107Recurse {
+
+
+
+}
