c6fa24603058c1048a9d1c93778c4a5a8469ec95
tree0122 <cuiyaohua11@163.com>
2020-03-23 16:06:43 +0800

    leetcode: linked list circle

---
+++ src/main/java/com/tree/core/algorithm/leetcode/P0141LinkedListCircle.java
@@ -0,0 +1,59 @@
+package com.tree.core.algorithm.leetcode;
+
+import java.util.HashSet;
+
+/**
+ * Given a linked list, determine if it has a cycle in it.
+ *
+ * To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where tail connects to the second node.
+ */
+public class P0141LinkedListCircle {
+
+    public boolean hasCycle(ListNode head){
+        if (head == null){
+            return true;
+        }
+        HashSet<ListNode> set = new HashSet<>();
+        while (head != null){
+            if (set.contains(head)){
+                return true;
+            }
+            set.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+
+    public boolean slowAndFast(ListNode head){
+        if (head == null){
+            return false;
+        }
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+}
