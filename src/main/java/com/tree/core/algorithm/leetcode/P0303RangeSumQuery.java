d30039f388f5261b71bfaa06e919d4b4414f268f
tree0122 <cuiyaohua11@163.com>
2020-04-13 17:44:06 +0800

    leetcode: range sum query immutable

---
+++ src/main/java/com/tree/core/algorithm/leetcode/P0303RangeSumQuery.java
@@ -0,0 +1,37 @@
+package com.tree.core.algorithm.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+
+ Example:
+ Given nums = [-2, 0, 3, -5, 2, -1]
+
+ sumRange(0, 2) -> 1
+ sumRange(2, 5) -> -1
+ sumRange(0, 5) -> -3
+ Note:
+ You may assume that the array does not change.
+ There are many calls to sumRange function.
+ */
+public class P0303RangeSumQuery {
+
+    private int[] sum;
+
+    public P0303RangeSumQuery(int[] nums) {
+        sum = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            sum[i + 1] = sum[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sum[j + 1] - sum[i];
+    }
+
+}
